e4873a2bec1250d6b894959e2e74d21e3def1f96
swikenson <wikensonsimond@hotmail.com>
2019-01-17 10:28:05 +0100

    Ceci-est-mon-premier-commit-avec-git

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Wikenson SIMOND</title>    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<p>Je suis dans ma page contact</p>
+
+</body>
+
+</html>
